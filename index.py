145453c6fdcd622c2726b6e43e061af810b15dee
Chris Ramsden <1986chris@gmail.com>
2015-12-12 23:16:17 +0000

    Delete pymongo comments and redundant import

--- index.py
+++ index.py
@@ -1,26 +1,8 @@
 import bottle
-import pymongo
-
-# this is the handler for the default path of the web server
 
 @bottle.route('/')
 def index():
-
-    # connect to mongoDB
-    #connection = pymongo.MongoClient('localhost', 27017)
-
-    # attach to test database
-    #db = connection.test
-
-
-    # get handle for names collection
-    #name = db.names
-
-    # find a single document
-    #item = name.find_one()
-
-    #return '<b>Hello %s!</b>' % item['name']
-    return '<b>Hollass</b>'
+    return '<b>Hola</b>'
 
 
 bottle.run(host='127.0.0.1', port=8080)
